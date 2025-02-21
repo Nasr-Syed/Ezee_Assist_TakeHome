77ac4bd5edfcf46bb3cd42fd2202523acdcfa7c7
Nasr Syed <nasrsyed20@gmail.com>
2025-02-21 02:46:24 -0500

    final

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from urllib.parse import urljoin, urlparse
 
 # Define the base URL
 main_url = "https://mineolasearchpartners.com"
+text_folder = r"C:\Users\nasrs\Documents\Projects\Ezee\text"
 image_folder = r"C:\Users\nasrs\Documents\Projects\Ezee\images"
 visited_links = set()  # To track visited URLs. Set data type is used to make sure there are unique links.
 total_visited_images = set()
@@ -18,19 +19,25 @@ summary_data = {}  # Store summary info
 def download(image_link):
     if image_link in total_visited_images:
         return
-    filename = os.path.join(image_folder, os.path.basename(image_link))
+    filename = os.path.basename(urlparse(image_link).path)
+    filepath = os.path.join(image_folder, filename)
     try:
-        response = requests.get(image_link)
+        response = requests.get(image_link, stream=True)
+        print(filename)
+        print(response.status_code)
         if response.status_code == 200:
-            with open(filename, 'wb') as file:
+            with open(filepath, 'wb') as file:
                 file.write(response.content)
-            print(f"Downloaded {image_link}")
+            print(f"Downloaded {filepath}")
             total_visited_images.add(image_link)  # Track downloaded images
         else:
+            print(f"Response code is: {response.status_code}")
             print(f"Failed to download {image_link}")
     except Exception as e:
         print(f"Failed to download url {image_link}")
 
+def clean(url):
+    return url.replace("https://", "").replace("http://", "").replace("/", "_").replace("?", "_")
 
 def scrape_page(url):
     if url in visited_links:
@@ -49,17 +56,21 @@ def scrape_page(url):
         soup = BeautifulSoup(content, "html.parser")
 
         # Extract text
+        filename = os.path.join(text_folder, clean(url) +".txt")
         page_text = soup.get_text(separator=" ", strip=True)
         print("Printing page content...")
         print(page_text, "\n")
         # Write text to .txt file
+        with open(filename, "w") as file:
+            file.write(page_text)
+            print(f"Wrote text to file: {filename}")
+
 
         # Extract images
         page_images = soup.find_all("img", src=True)
         for img in page_images:
             image_link = urljoin(main_url, img["src"])
-            if urlparse(image_link).netloc == urlparse(main_url).netloc:
-                download(image_link)
+            download(image_link)
 
         # Recursively finding URL links inside main URL
         all_urls = soup.find_all("a", href=True)
@@ -92,6 +103,6 @@ with open("summary.json", "w") as f:
 
 # Summary
 print("\nTotal pages scraped:", len(visited_links))
-print("\nList of image URLs accessed:", total_visited_images)
 print("Pages visited:", visited_links)
+print("\nList of Total image URLs accessed:", total_visited_images)
 print("Summary added to: summary.json")
